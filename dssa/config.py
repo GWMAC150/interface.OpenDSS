be194fa56ccaa1962d910a9fa18c2372db95f85e
purboday <32465775+purboday@users.noreply.github.com>
2020-02-17 12:57:45 -0600

    OpenDSS agent code

---
+++ dssa/config.py
@@ -0,0 +1,65 @@
+'''
+Created on Feb 13, 2020
+
+@author: ghoshp
+'''
+
+import yaml
+import re
+
+class Config(yaml.YAMLObject):
+    '''
+    dssa configuration object
+    '''
+    yaml_loader = yaml.SafeLoader
+    yaml_tag = u'!DSSAConfig'
+    def __init__(self,
+                 host='',port=0,
+                 time_base = '2000-01-01',
+                 dbhost='localhost', dbport=8086, dbuser='riapsdev', dbpassword='riaps', dbname='opendss', dbdrop=True, stepsize = '15m',
+                 mode = 'daily', number = 96, time_delta = '1s'):
+        self.host = host
+        self.port = port
+#        self.time_stop = time_stop
+#        self.time_pace = time_pace
+        self.time_base = time_base
+        self.dbhost = dbhost
+        self.dbport = dbport
+        self.dbuser = dbuser
+        self.dbpassword = dbpassword
+        self.dbname = dbname
+        self.dbdrop = dbdrop
+        self.stepsize = stepsize
+        self.mode = mode
+        self.number = number
+        self.time_delta = self.convert_duration(time_delta)
+        
+    def __repr__(self):
+        return "%s(host=%r,port=%r,dbhost=%r,dbport=%r,dbuser=%r,dbpassword=%r,dbname=%r,dbdrop=%r,stepsize=%r,mode=%r,number=%r,time_delta=%r)" % \
+            (self.__class__.__name__,
+             self.host,self.port, 
+             self.dbhost,self.dbport,self.dbuser,self.dbpassword,self.dbname,self.dbdrop,
+             self.stepsize,self.mode,self.number, self.time_delta
+             )
+    def convert_duration(self,val):
+        pat = re.compile(r'([hmsd])')
+        interval,unit,drop=pat.split(val)
+        if unit == 's':
+            return int(interval)
+        elif unit == 'm':
+            return int(interval)*60
+        elif unit == 'h':
+            return int(interval)*3600
+        elif unit == 'd':
+            return int(interval)*24*3600
+        else:
+            return None
+    
+    @classmethod
+    def setup(cls):
+        def constructor(loader, node) :
+            fields = loader.construct_mapping(node)
+            return Config(**fields)
+        yaml.add_constructor('!DSSAConfig', constructor)
+        
+
---
+++ dssa/main.py
@@ -0,0 +1,46 @@
+'''
+Created on Feb 2, 2020
+
+@author: ghoshp
+'''
+import sys
+import os
+import argparse
+import traceback
+import signal
+
+from dssa.agent import Agent
+
+theAgent = None
+conf = None
+
+def termHandler(_signal,_frame):
+    global theAgent
+    theAgent.stop()
+    
+####################
+
+def main(debug=True):
+    parser = argparse.ArgumentParser(description="OpenDSS agent")
+    parser.add_argument("path",help="path to model directory")
+    parser.add_argument("name",help="base name for .dss, .dsl files")
+    args = parser.parse_args()
+        
+    signal.signal(signal.SIGTERM,termHandler)
+    signal.signal(signal.SIGINT,termHandler)
+    
+    global theAgent
+    try:
+        os.chdir(args.path)
+        theAgent = Agent(args.name)
+        theAgent.start()       
+        theAgent.run()       
+        theAgent.stop()
+    except Exception:
+        traceback.print_exc()
+        if theAgent != None: theAgent.stop()
+    #print ("Unexpected error:", sys.exc_info()[0])
+    sys.exit()
+    
+if __name__ == '__main__':
+    main()
