827aeb2ae19c2ece49c0deb9b8d8ed8d38da32ab
purboday <purboday.ghosh@vanderbilt.edu>
2020-03-29 23:43:07 -0500

    added packaging configurations

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='OpenDSSAgent',
+      version='0.1',
+      description='OpenDSS agent for RIAPS',
+      url='https://github.com/RIAPS/interface.OpenDSS',
+      author='riaps',
+      author_email='riaps@isis.vanderbilt.edu',
+      license='MIT',
+      packages=['dssa'],
+      zip_safe=False)
